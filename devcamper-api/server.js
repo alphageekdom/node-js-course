ffc335cb90f5b3e853680ac4835418de1534d209
alphageekdom <97497295+alphageekdom@users.noreply.github.com>
2023-10-19 09:17:31 -0700

    create own middleware and morgan middleware

--- devcamper-api/server.js
+++ devcamper-api/server.js
@@ -1,11 +1,17 @@
 const express = require('express');
 require('dotenv').config({ path: './config/config.env' });
+const morgan = require('morgan');
 const port = process.env.PORT || 5000;
 
 // Route files
 const bootcamps = require('./routes/bootcamps');
 
 const app = express();
+
+// Dev logging middleware
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
 
 // Mount routers
 app.use('/api/v1/bootcamps', bootcamps);
