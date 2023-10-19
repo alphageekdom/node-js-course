656a7b469959dacdb1abff7988b454c061d0f001
alphageekdom <97497295+alphageekdom@users.noreply.github.com>
2023-10-19 10:38:03 -0700

    bootcamp model

--- devcamper-api/server.js
+++ devcamper-api/server.js
@@ -3,6 +3,7 @@ require('dotenv').config({ path: './config/config.env' });
 const morgan = require('morgan');
 const port = process.env.PORT || 5000;
 const connectDB = require('./config/db');
+const colors = require('colors');
 
 connectDB();
 
@@ -20,12 +21,15 @@ if (process.env.NODE_ENV === 'development') {
 app.use('/api/v1/bootcamps', bootcamps);
 
 const server = app.listen(port, () =>
-  console.log(`Server running in ${process.env.NODE_ENV} mode on port: ${port}`)
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode on port: ${port}`.yellow
+      .bold
+  )
 );
 
 // Handle unhandled promise rejections
 process.on('unhandledRejection', (err, promise) => {
-  console.log(`Error: ${err.message}`);
+  console.log(`Error: ${err.message}`.red.bold);
 
   // Close server & exit process
   server.close(() => process.exit(1));
