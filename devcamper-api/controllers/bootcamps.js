16edcb73dd4c5f923bceb10f8909b87981f0195f
alphageekdom <97497295+alphageekdom@users.noreply.github.com>
2023-10-19 12:12:57 -0700

    PUT - update bootcamps to db

--- devcamper-api/controllers/bootcamps.js
+++ devcamper-api/controllers/bootcamps.js
@@ -57,10 +57,26 @@ exports.createBootcamp = async (req, res, next) => {
 // @desc    Update bootcamp
 // @route   PUT /api/v1/bootcamps/:id
 // @access  Private
-exports.updateBootcamp = (req, res, next) => {
-  res
-    .status(200)
-    .json({ success: true, msg: `Update bootcamp ${req.params.id}` });
+exports.updateBootcamp = async (req, res, next) => {
+  try {
+    const bootcamp = await Bootcamp.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true,
+    });
+
+    if (!bootcamp) {
+      return res.status(400).json({ success: false });
+    }
+
+    res.status(200).json({
+      success: true,
+      msg: `Update bootcamp ${bootcamp.id}`,
+      data: bootcamp,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ success: false, error: 'Something went wrong' });
+  }
 };
 
 // @desc    Delete bootcamp
