262256df9632df68698cd80bc71823e465404b9c
alphageekdom <97497295+alphageekdom@users.noreply.github.com>
2023-10-19 08:41:51 -0700

    created routes

--- devcamper-api/server.js
+++ devcamper-api/server.js
@@ -2,7 +2,13 @@ const express = require('express');
 require('dotenv').config({ path: './config/config.env' });
 const port = process.env.PORT || 5000;
 
+// Route files
+const bootcamps = require('./routes/bootcamps');
+
 const app = express();
+
+// Mount routers
+app.use('/api/v1/bootcamps', bootcamps);
 
 app.listen(port, () =>
   console.log(`Server running in ${process.env.NODE_ENV} mode on port: ${port}`)
