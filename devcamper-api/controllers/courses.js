f50bd1f9e6c3fa513f843c2f1364a88279cb94b1
alphageekdom <97497295+alphageekdom@users.noreply.github.com>
2023-10-19 22:08:13 -0700

    update and delete course

--- devcamper-api/controllers/courses.js
+++ devcamper-api/controllers/courses.js
@@ -8,17 +8,21 @@ const Bootcamp = require('../models/Bootcamp');
 // @route   GET /api/v1/bootcamps/:bootcampID/courses
 // @access  Public
 exports.getCourses = asyncHandler(async (req, res, next) => {
-  if (req.params.bootcampId) {
-    const courses = await Course.find({ bootcamp: req.params.bootcampId });
+  let query;
 
-    return res.status(200).json({
-      success: true,
-      count: courses.length,
-      data: courses,
-    });
+  if (req.params.bootcampId) {
+    query = Course.find({ bootcamp: req.params.bootcampId });
   } else {
-    res.status(200).json(res.advancedResults);
+    query = Course.find();
   }
+
+  const courses = await query;
+
+  res.status(200).json({
+    success: true,
+    count: courses.length,
+    data: courses,
+  });
 });
 
 // @desc    Get a single course
@@ -47,6 +51,7 @@ exports.getCourse = asyncHandler(async (req, res, next) => {
 // @access  Private
 exports.addCourse = asyncHandler(async (req, res, next) => {
   req.body.bootcamp = req.params.bootcampId;
+  req.body.user = req.user.id;
 
   const bootcamp = await Bootcamp.findById(req.params.bootcampId);
 
@@ -64,5 +69,48 @@ exports.addCourse = asyncHandler(async (req, res, next) => {
   res.status(200).json({
     success: true,
     data: course,
+  });
+});
+
+// @desc    Update a course
+// @route   Post /api/v1/courses/:id
+// @access  Private
+exports.updateCourse = asyncHandler(async (req, res, next) => {
+  let course = await Course.findById(req.params.id);
+
+  if (!course) {
+    return next(
+      new ErrorResponse(`No bootcamp with the id of ${req.params.id}`, 404)
+    );
+  }
+
+  course = await Course.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true,
+  });
+
+  res.status(200).json({
+    success: true,
+    data: course,
+  });
+});
+
+// @desc    Delete a course
+// @route   DELETE /api/v1/courses/:id
+// @access  Private
+exports.deleteCourse = asyncHandler(async (req, res, next) => {
+  const course = await Course.findById(req.params.id);
+
+  if (!course) {
+    return next(
+      new ErrorResponse(`No bootcamp with the id of ${req.params.id}`, 404)
+    );
+  }
+
+  await course.deleteOne();
+
+  res.status(200).json({
+    success: true,
+    data: {},
   });
 });
